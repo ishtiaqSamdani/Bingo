359e0779725f6e083a62731421f35cebf06982d6
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2022-02-25 02:26:48 -0800

    front and fire base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const arrayGadu = [...Array(26).keys()].slice(1, 26);
+  const [arr, setArr] = useState([]);
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    setArr(array);
+  };
+
+useEffect(() => {
+  shuffle(arrayGadu)
+}, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container">
+        {arr.map((ele) => (
+          <>
+            <div className="box" onClick={(e)=>e.target.style.background="grey"}>{ele}</div>
+          </>
+        ))}
+      </div>
+
+      <div className="btn_container">
+        <button
+          className="btn"
+          onClick={() => {
+            shuffle(arrayGadu);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Random
+        </button>
+      </div>
+    </>
   );
 }
 
